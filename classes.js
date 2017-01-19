101941e664c426b30e2d3f71c4d844772825bc13
todddrinkwater <todddrinkwaternz@gmail.com>
2017-01-20 08:28:43 +1300

    add green background colour to div two

--- classes.js
+++ classes.js
@@ -1,12 +1,12 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  one();
+  two();
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -19,6 +19,12 @@ function one () {
 }
 
 // CREATE FUNCTION two HERE
+
+function two(){
+  var two = document.getElementById('two');
+
+  two.classList.add('green');
+}
 
 // CREATE FUNCTION three HERE
 
