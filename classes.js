c73791c000921342e4cac6190f0e070db12d3e9e
todddrinkwater <todddrinkwaternz@gmail.com>
2017-01-20 08:58:11 +1300

    make fourth div visible

--- classes.js
+++ classes.js
@@ -8,6 +8,7 @@ function start () {
   one();
   two();
   three();
+  makeVisible();
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -34,3 +35,7 @@ three.classList.add('purple');
 };
 
 // CREATE FUNCTION makeVisible HERE
+function makeVisible(){
+  var makeVisible = document.getElementsByClassName('invisible')[0];
+  makeVisible.classList.add('visible');
+}
